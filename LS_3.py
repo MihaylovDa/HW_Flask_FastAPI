c077a777460b9579be0aa3e65e01f1f7a4a525d6
Danil <132139626+MihaylovDa@users.noreply.github.com>
2024-01-17 16:23:46 +0700

    Update LS_3.py

--- LS_3.py
+++ LS_3.py
@@ -1,43 +1,28 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash
+import bcrypt
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-db = SQLAlchemy(app)
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(100))
-    last_name = db.Column(db.String(100))
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-
+class User:
     def __init__(self, first_name, last_name, email, password):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        self.password = generate_password_hash(password)
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-        
-
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         email = request.form['email']
         password = request.form['password']
-        
-        user = User(first_name=first_name, last_name=last_name, email=email, password=password)
-        db.session.add(user)
-        db.session.commit()
-        return 'Пользователь зарегистрирован успешно!'
-    
+
+        user = User(first_name, last_name, email, password)
+        user.save_to_database()
+
+        return "Регистрация успешна!"
     return render_template('register.html')
 
-
 if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
+    app.run()
